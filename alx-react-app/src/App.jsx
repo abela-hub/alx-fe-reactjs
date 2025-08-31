a54ceb88d529f0431886c67bc5d981dcea18490d
abela-hub <derejeabel986@gmail.com>
2025-08-31 11:22:35 -0400

    Add React app created with Vite

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,9 +1,18 @@
 import WelcomeMessage from './components/WelcomeMessage';
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
 
 function App() {
   return (
     <div>
+      {/* Task 1: Show WelcomeMessage */}
       <WelcomeMessage />
+
+      {/* Task 2: Show Header, MainContent, Footer */}
+      <Header />
+      <MainContent />
+      <Footer />
     </div>
   );
 }
