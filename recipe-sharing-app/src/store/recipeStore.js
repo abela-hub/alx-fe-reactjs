ed5b63ca4f4086d36c0f894c5671393b3ed2b4a0
abela-hub <derejeabel986@gmail.com>
2025-09-13 15:37:29 -0400

    feat: init recipe-sharing-app with Zustand

--- recipe-sharing-app/src/store/recipeStore.js
+++ recipe-sharing-app/src/store/recipeStore.js
@@ -1,10 +1,9 @@
-import { create } from 'zustand';
+// src/store/recipeStore.js
+import create from 'zustand';
 
 export const useRecipeStore = create((set) => ({
     recipes: [],
     addRecipe: (newRecipe) =>
-        set((state) => ({
-            recipes: [...state.recipes, newRecipe],
-        })),
+        set((state) => ({ recipes: [...state.recipes, newRecipe] })),
     setRecipes: (recipes) => set({ recipes }),
 }));
