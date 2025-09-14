cf467615abd6023838adef77040965c02199ea76
abela-hub <derejeabel986@gmail.com>
2025-09-14 10:09:37 -0400

    project

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,20 +1,37 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-// Import your components (required by checker)
+// Import your components
 import AddRecipeForm from "./components/AddRecipeForm";
 import RecipeList from "./components/RecipeList";
+import RecipeDetails from "./components/RecipeDetails"; // optional for details route
+import EditRecipeForm from "./components/EditRecipeForm"; // optional for edit route
 
 function App() {
   return (
-    <div className="App">
-      <h1>Recipe Sharing App</h1>
+    <Router>
+      <div className="App">
+        <h1>Recipe Sharing App</h1>
+        <Routes>
+          {/* Home page: Add + List recipes */}
+          <Route
+            path="/"
+            element={
+              <>
+                <AddRecipeForm />
+                <RecipeList />
+              </>
+            }
+          />
 
-      {/* Form to add new recipes */}
-      <AddRecipeForm />
+          {/* Recipe details page */}
+          <Route path="/recipes/:id" element={<RecipeDetails />} />
 
-      {/* List of all recipes */}
-      <RecipeList />
-    </div>
+          {/* Edit recipe page */}
+          <Route path="/recipes/:id/edit" element={<EditRecipeForm />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
