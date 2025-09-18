fdd726d5faca25c5215b5d9a21e97c544c3cabf4
abela-hub <derejeabel986@gmail.com>
2025-09-18 03:06:00 -0400

    react project

---
+++ my-company/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+function Navbar() {
+    return (
+        <nav style={{ background: "#333", padding: "10px" }}>
+            <Link to="/" style={{ margin: "0 10px", color: "white" }}>Home</Link>
+            <Link to="/about" style={{ margin: "0 10px", color: "white" }}>About</Link>
+            <Link to="/services" style={{ margin: "0 10px", color: "white" }}>Services</Link>
+            <Link to="/contact" style={{ margin: "0 10px", color: "white" }}>Contact</Link>
+        </nav>
+    );
+}
+export default Navbar;
