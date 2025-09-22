aa517ed025e1264686496a7fcc4f39b354586dec
abela-hub <derejeabel986@gmail.com>
2025-09-22 11:20:29 -0400

    react project

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,9 +1,11 @@
-export default function App() {
+import UserProfile from './components/UserProfile'
+
+function App() {
   return (
-    <div className="h-screen flex items-center justify-center bg-gray-900">
-      <h1 className="text-4xl font-bold text-white">
-        🚀 Tailwind is Working!
-      </h1>
+    <div>
+      <UserProfile />
     </div>
-  );
+  )
 }
+
+export default App
