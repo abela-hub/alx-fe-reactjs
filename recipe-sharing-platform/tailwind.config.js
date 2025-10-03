223f095c38c82c7eb84ecd43d3d499adc2c69c2d
abela-hub <derejeabel986@gmail.com>
2025-10-03 09:04:24 -0400

    react project

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
     content: [
         "./index.html",
+        "./public/index.html",
         "./src/**/*.{js,jsx,ts,tsx}",
     ],
     theme: {
